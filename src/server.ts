17fc41e2f181695a9fa4a0517fa5b356309f313c
lorstenoplo <nishanthdipali@gmail.com>
2024-09-15 19:34:29 +0530

    added collab editing

--- src/server.ts
+++ src/server.ts
@@ -6,17 +6,28 @@ import cors from "cors";
 const app = express();
 const server = http.createServer(app);
 app.use(cors());
-const io = new Server(server,{
+const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3001",
+    origin: "*",
     methods: ["GET", "POST"],
   },
 });
 
-
 // Handle socket connections
 io.on("connection", (socket) => {
   console.log("a user connected");
+
+  // Collaborative editing
+
+  socket.on("join-room", (fileId, userId) => {
+    socket.join(fileId);
+    console.log("joined room", fileId, userId);
+
+    socket.on("send-changes", (delta) => {
+      console.log("received changes", delta);
+      socket.broadcast.to(fileId).emit("receive-changes", delta);
+    });
+  });
 
   // Listen for messages
   socket.on("message", (msg) => {
@@ -32,7 +43,7 @@ io.on("connection", (socket) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
